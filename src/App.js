6cd3360bc0e6ddd5c0c444ede2397ebbc0cb082c
Pakize Bozkurt <pakiozihak@gmail.com>
2022-12-07 21:45:24 +0000

    Creating Countries app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+
+//"https://restcountries.com/v2/all"
+function App() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/CardCountry.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+
+
+function CardCountry() {
+const [country, setCountry] = useState([]);
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CardCountry;
